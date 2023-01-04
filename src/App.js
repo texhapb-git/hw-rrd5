8d71207a7ee768f6542a9ebf755197df88dca7f9
TexHaPb <texhapb@bk.ru>
2023-01-04 14:29:23 +0300

    add routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import UsersPage from "./pages/UsersPage";
 
 
 function App() {
   return (
-    <div>
-      Hello
-    </div>
+    <BrowserRouter>
+      <div className="app">
+        <Switch>
+          <Route path="/" exact component={HomePage} />
+          <Route path="/users" component={UsersPage} />
+          <Redirect to="/" />
+        </Switch>
+      </div>
+    </BrowserRouter >
+
   );
 }
 
